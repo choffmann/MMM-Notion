b5785833742c000713c485ce76353a1ceb3d5a58
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2023-03-06 17:46:47 +0100

    Create ListView with Title, Emoji and Checkbox

--- MMM-Notion.js
+++ MMM-Notion.js
@@ -28,7 +28,7 @@ Module.register("MMM-Notion", {
 
 	start: function () {
 		var self = this;
-		this.text = "";
+		this.databases = [];
 
 		this.sendSocketNotification("HERE_IS_YOUR_CONFIG", this.config);
 		setInterval(function () {
@@ -40,12 +40,83 @@ Module.register("MMM-Notion", {
 	getDom: function () {
 		// create element wrapper for show into the module
 		const wrapper = document.createElement("div");
-		wrapper.id = "notion-table";
-		wrapper.innerText = this.text
+		wrapper.id = "mmm-notion";
+		this.databases.forEach(database => {
+			this.createListView(wrapper, database)
+		})
 		return wrapper;
 	},
 
-	getScripts: function() {
+	createListView: function (wrapper, properties) {
+		// Name.........................Tag Tag
+		const list = document.createElement("div")
+		wrapper.appendChild(list)
+		list.id = "mmm-notion-listview"
+
+		properties.data.forEach(prop => {
+			const pageElement = document.createElement("div")
+			pageElement.id = "mmm-notion-listview-element"
+
+			this.createTitleContainer(pageElement, prop)
+			this.createPropertiesContainer(pageElement, prop, properties.properties)
+
+			list.appendChild(pageElement)
+		})
+	},
+
+	findTitleProp: function (properties) {
+		for (const key in properties) {
+			if (properties.hasOwnProperty.call(properties, key) && properties[key].hasOwnProperty('type') && properties[key].type === "title") {
+				return properties[key].title[0].text.content
+			}
+		}
+	},
+
+	createTitleContainer: function (wrapper, props) {
+		const titleContainer = document.createElement("div")
+		titleContainer.id = "mmm-notion-listview-titleContainer"
+		this.createTitleEmoji(titleContainer, props.icon.emoji)
+		this.createTitle(titleContainer, this.findTitleProp(props.properties))
+		wrapper.appendChild(titleContainer)
+	},
+
+	createTitle: function (wrapper, title) {
+		const titleDom = document.createElement("div")
+		titleDom.id = "mmm-notion-listview-title"
+		titleDom.innerText = title
+		wrapper.appendChild(titleDom)
+	},
+
+	createTitleEmoji: function (wrapper, emoji) {
+		const emojiDom = document.createElement("div")
+		emojiDom.id = "mmm-notion-listview-emoji"
+		emojiDom.innerText = emoji
+		wrapper.appendChild(emojiDom)
+	},
+
+	createPropertiesContainer: function (wrapper, props, propNames) {
+		const propContainer = document.createElement("div")
+		propContainer.id = "mmm-notion-listview-propContainer"
+		console.log(props)
+		propNames.forEach(propName => {
+			switch (props.properties[propName].type) {
+				case "checkbox":
+					this.createCheckbox(propContainer, props.properties[propName].checkbox)
+					break;
+			}
+		})
+		wrapper.appendChild(propContainer)
+	},
+
+	createCheckbox: function (wrapper, value) {
+		const checkbox = document.createElement("input")
+		checkbox.id = "mmm-notion-listview-checkbox"
+		checkbox.setAttribute("type", "checkbox")
+		checkbox.checked = value
+		wrapper.appendChild(checkbox)
+	},
+
+	getScripts: function () {
 		return [];
 	},
 
@@ -56,7 +127,7 @@ Module.register("MMM-Notion", {
 	},
 
 	// Load translations files
-	getTranslations: function() {
+	getTranslations: function () {
 		//FIXME: This can be load a one file javascript definition
 		return {
 			en: "translations/en.json",
@@ -66,8 +137,8 @@ Module.register("MMM-Notion", {
 
 	// socketNotificationReceived from helper
 	socketNotificationReceived: function (notification, payload) {
-		if(notification === "MMM-Notion-DATABASE-DATA") {
-			this.text = payload;
+		if (notification === "MMM-Notion-DATABASE-DATA") {
+			this.databases = payload;
 			this.updateDom();
 		}
 	},
